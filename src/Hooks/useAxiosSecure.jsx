a318cf3ca75132c6733864128a7eef5637d11609
@maptaul <maptaulislam1@gmail.com>
2025-01-10 17:24:35 +0600

    add

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,6 +4,17 @@ const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 const useAxiosSecure = () => {
+  //request interceptor to add authorization for every secure call to the api
+  axiosSecure.interceptors.request.use(function (config) {
+    const token = localStorage.getItem("access-token");
+    console.log("request stop by interceptors", token);
+    config.headers.authorization = `Bearer ${token}`;
+    return config;
+  }),
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    };
   return axiosSecure;
 };
 
