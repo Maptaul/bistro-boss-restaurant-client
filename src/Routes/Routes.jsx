631ce2000b936de36e6ceef43286df805aac0237
@maptaul <maptaulislam1@gmail.com>
2025-01-14 01:02:40 +0600

    add

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -8,6 +8,7 @@ import Payment from "../Pages/Dashboard/Payment/Payment";
 import UpdateItem from "../Pages/Dashboard/UpdateItem/UpdateItem";
 import AddItems from "../Pages/Dashboard/addItems/AddItems";
 import ManageItems from "../Pages/Dashboard/manageItems/ManageItems";
+import PaymentHistory from "../Pages/Dashboard/paymentHistory/PaymentHistory";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import Menu from "../Pages/Menu/Menu/Menu";
@@ -68,6 +69,10 @@ export const router = createBrowserRouter([
       {
         path: "payment",
         element: <Payment />,
+      },
+      {
+        path: "paymentHistory",
+        element: <PaymentHistory />,
       },
 
       // admin routes
