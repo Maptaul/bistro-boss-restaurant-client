d1064b283b6154d9c27f8ccb10dd85ef053cba14
@maptaul <maptaulislam1@gmail.com>
2025-01-28 20:47:40 +0600

    final

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -114,7 +114,7 @@ export const router = createBrowserRouter([
           </AdminRoute>
         ),
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/menu/${params.id}`),
+          fetch(`https://y-nine-tawny.vercel.app/menu/${params.id}`),
       },
       {
         path: "users",
